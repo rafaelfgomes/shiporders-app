c7eab8aed30c276e57d2836d91a9a7ef1f14d762
Rafael Gomes <faelfg@gmail.com>
2021-01-15 03:03:11 -0300

    Adjust env file and config.sh

--- config.sh
+++ config.sh
@@ -5,25 +5,11 @@ if [ -f .env ]; then
 
     LINE="============================================="
 
-    echo -e "${LINE}"
-    echo -e "App build and config"
-    echo -e "${LINE}"
-
-    echo -e "\n"
-
-    #folderName=$(basename $(pwd))
-    prefixContainerName=shiporders
-
-    apiContainerName=$prefixContainerName"_api"
-    pageContaineNrame=$prefixContainerName"_frontend"
-
-    echo "$LINE"
+    echo -e "$LINE"
     echo -e "Application initial config"
-    echo "$LINE\n"
+    echo -e "$LINE\n"
 
     read -p "Database root password: " dbRootPass
-    read -p "Database username: " dbUsername
-    read -p "Database user password: " dbUserPass
     read -p "Database name: " dbName
 
     echo -e "\nChoose the environment type"
@@ -44,8 +30,6 @@ if [ -f .env ]; then
 
     sed -i "s+APP_ENV=+APP_ENV=$appEnv+g" .env
     sed -i "s+DB_ROOT_PASSWD=+DB_ROOT_PASSWD=$dbRootPass+g" .env
-    sed -i "s+DB_USER=+DB_USER=$dbUsername+g" .env
-    sed -i "s+DB_PASSWD=+DB_PASSWD=$dbUserPass+g" .env
     sed -i "s+DB_NAME=+DB_NAME=$dbName+g" .env
 
     echo -e "\nFinished config"
